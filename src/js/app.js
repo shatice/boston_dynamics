0c20c0221d84ff7590f18411f39acc34d61dfee6
SHatice <sarah.gungor@hetic.net>
2019-04-01 01:29:17 +0200

    inital commit, forked from Jean, add npm/parcel

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+// var nav = document.querySelector('nav ul');
+// var open = document.querySelector('.header__open');
+
+// open.addEventListener('click', function(){
+//   console.log('sffd')
+//   nav.classList.toggle('nav__is-open');
+// })
